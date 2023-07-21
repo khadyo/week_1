3e2210093109d2f273931f36610bced88c9e3c23
kadyo <khadija.engsf@gmail.com>
2023-07-21 22:12:39 +0200

    thirth problm 15 ms

---
+++ Fafa-and-his-Company/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std; 
+int main(){
+    
+    int a,count=0;
+    cin>> a; 
+    for(int i=1;i<a;i++){
+        if(a%i==0){
+            count ++;
+        }
+    }
+    cout<<count; 
+
+    return 0; 
+}
